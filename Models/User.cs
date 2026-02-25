2bce5efb07ed85ff4aadd6ffaa95154a55068b0f
Aparnanil <aparnanil2007@gmail.com>
2026-02-25 22:51:35 +0530

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace UserManagementAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Department { get; set; }
+    }
+}
